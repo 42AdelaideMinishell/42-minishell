64456ebcaed5b7a517fd59d809eb3c6f2a190d1b
Jaejun Shin <jaeshinkorean@gmail.com>
2023-08-31 18:46:03 +0930

    ctrl+c ctrl+"

--- inc/minishell.h
+++ inc/minishell.h
@@ -2,8 +2,17 @@
 # define MINISHELL_H
 
 # include "../libft/libft.h"
+# include <dirent.h>
 # include <stdio.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <termios.h>
+# include <termcap.h>
+# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+char	*g_input;
 
 #endif
